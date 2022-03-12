672c445336c7786b1c7b0c1bf0ab8dff74bce254
Jenny Hudson <jennykatherinehudson@gmail.com>
2022-03-13 00:10:48 +0100

    create __init_ and amount_of_files

--- FileDiscovery.py
+++ FileDiscovery.py
@@ -2,4 +2,9 @@ import glob
 import os
 
 class FileDiscovery:
-    
+
+    def __init__(self, dir):
+        self.dir = dir 
+
+    def amount_of_files(self):
+        self.amount_of_files = len(os.listdir(self.dir))
