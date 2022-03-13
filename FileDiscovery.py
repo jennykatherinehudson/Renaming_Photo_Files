0378d99c66076a3db240f313f59f531d1ea17f9e
Jenny Hudson <jennykatherinehudson@gmail.com>
2022-03-13 01:45:51 +0100

    creatng methods: all_extentions_in_dir, amount_of_specify_extention_dict

--- FileDiscovery.py
+++ FileDiscovery.py
@@ -1,10 +1,36 @@
 import glob
 import os
 
+
 class FileDiscovery:
 
     def __init__(self, dir):
-        self.dir = dir 
+        self.dir = dir
 
     def amount_of_all_files_in_dir(self):
-        self.amount_of_files = len(os.listdir(self.dir))
+        self.all_files_in_dir = os.listdir(self.dir)
+        return len(self.all_files_in_dir)
+
+    def all_extentions_in_dir(self):
+        splitted_files_list = [i.split('.') for i in self.all_files_in_dir]
+        self.all_extentions_set = set({})
+        for i in splitted_files_list:
+            self.all_extentions_set.add(i[1])
+        return self.all_extentions_set
+
+    def amount_of_specify_extention_dict(self):
+        extention_name = ['*.' + str(i) for i in self.all_extentions_set]
+        amount_of_specify_extention_list = [len(glob.glob(os.path.join(self.dir, i))) for i in extention_name]
+        amount_of_specify_extention_dict = {i : j for i,j in zip(extention_name, amount_of_specify_extention_list)}
+        return amount_of_specify_extention_dict
+    
+
+
+
+'''path = r'E:\Python\python_my_projekt1_renaming_photos\Library'
+
+photo_files_test = FileDiscovery(path)
+print(photo_files_test.amount_of_all_files_in_dir())
+print(photo_files_test.all_files_in_dir)
+print(photo_files_test.all_extentions_in_dir())
+photo_files_test.amount_of_specify_extention_dict()'''
--- working_file.py
+++ working_file.py
@@ -4,13 +4,13 @@ from FileDiscovery import FileDiscovery
 
 path = r'E:\Python\python_my_projekt1_renaming_photos\Library'
 
-photo_files_test = FileDiscovery(path)
-print(photo_files_test.amount_of_all_files_in_dir())
+photo_file = FileDiscovery(path)
+
+print('-'*50)
+print('''Files in the directory: {}
+In this direcory you have {} files.
+This is {} different extentions.'''.format(path, photo_file.amount_of_all_files_in_dir(), photo_file.all_extentions_in_dir()))
+
+print('In your directory there is: {}'.format(photo_file.amount_of_specify_extention_dict()))
 
 
-print(os.listdir(path))
-
-file_extension = '*.jpeg'
-path_with_extention = os.path.join(path, file_extension)
-
-glob_len_items = len(glob.glob(path_with_extention))
