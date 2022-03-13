be672015799a37075bdc47f0931dae36ec8f053e
Jenny Hudson <jennykatherinehudson@gmail.com>
2022-03-13 02:40:22 +0100

    add __extention_working

--- FileDiscovery.py
+++ FileDiscovery.py
@@ -1,36 +1,38 @@
 import glob
 import os
 
-
 class FileDiscovery:
 
     def __init__(self, dir):
         self.dir = dir
+        self.__extention_working()
 
-    def amount_of_all_files_in_dir(self):
-        self.all_files_in_dir = os.listdir(self.dir)
-        return len(self.all_files_in_dir)
+    def __extention_working(self):
+        #for number_files_in_dir
+        self.__all_files_in_dir = os.listdir(self.dir)
+        #for all_extentions_in_dir
+        self.__splitted_files_list = set(
+            [i.split('.')[1] for i in self.__all_files_in_dir])
+        #for unique_extention
+        extention_name = ['*.' + str(i) for i in self.__splitted_files_list]
+        unique_extention_list = [
+            len(glob.glob(os.path.join(self.dir, i))) for i in extention_name]
+        self.__unique_extention_dict = {i: j for i, j in zip(
+            extention_name, unique_extention_list)}
+
+    def number_files_in_dir(self):
+        return len(self.__all_files_in_dir)
 
     def all_extentions_in_dir(self):
-        splitted_files_list = [i.split('.') for i in self.all_files_in_dir]
-        self.all_extentions_set = set({})
-        for i in splitted_files_list:
-            self.all_extentions_set.add(i[1])
-        return self.all_extentions_set
+        return self.__splitted_files_list
 
-    def amount_of_specify_extention_dict(self):
-        extention_name = ['*.' + str(i) for i in self.all_extentions_set]
-        amount_of_specify_extention_list = [len(glob.glob(os.path.join(self.dir, i))) for i in extention_name]
-        amount_of_specify_extention_dict = {i : j for i,j in zip(extention_name, amount_of_specify_extention_list)}
-        return amount_of_specify_extention_dict
-    
+    def unique_extention(self):
+        return self.__unique_extention_dict
 
 
-
-'''path = r'E:\Python\python_my_projekt1_renaming_photos\Library'
+""" path = r'E:\Python\python_my_projekt1_renaming_photos\Library'
 
 photo_files_test = FileDiscovery(path)
-print(photo_files_test.amount_of_all_files_in_dir())
-print(photo_files_test.all_files_in_dir)
+print(photo_files_test.number_files_in_dir())
 print(photo_files_test.all_extentions_in_dir())
-photo_files_test.amount_of_specify_extention_dict()'''
+print(photo_files_test.unique_extention()) """
--- working_file.py
+++ working_file.py
@@ -4,13 +4,14 @@ from FileDiscovery import FileDiscovery
 
 path = r'E:\Python\python_my_projekt1_renaming_photos\Library'
 
-photo_file = FileDiscovery(path)
+photo_files = FileDiscovery(path)
 
 print('-'*50)
 print('''Files in the directory: {}
 In this direcory you have {} files.
-This is {} different extentions.'''.format(path, photo_file.amount_of_all_files_in_dir(), photo_file.all_extentions_in_dir()))
+There are {} different extentions.'''.format(path, photo_files.number_files_in_dir(), photo_files.all_extentions_in_dir()))
 
-print('In your directory there is: {}'.format(photo_file.amount_of_specify_extention_dict()))
+print('In your directory there are: {}'.format(photo_files.unique_extention()))
+print('-'*50)
 
 
